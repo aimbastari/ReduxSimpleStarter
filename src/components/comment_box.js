08ad8431c7a00e7917245547ff170aedb6e780ea
aimbastari <aimbastari@gmail.com>
2016-04-17 23:44:49 -0700

    initial commit

---
+++ src/components/comment_box.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+
+export default class CommentBox extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {comment: ''};
+  }
+
+  handleChange(event){
+    this.setState({comment: event.target.value} );
+
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+
+
+  }
+
+  render() {
+    return (
+      <div className='comment-box'>
+        <textarea
+          value={this.state.comment}
+          onChange={this.handleChange.bind(this)} />
+        <button> Submit Comment </button>
+      </div>
+    );
+  }
+}
